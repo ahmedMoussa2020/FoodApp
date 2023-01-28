b627ab75eeb63c4fc58da9f511f41110b7817a62
Ahmed <>
2023-01-28 17:53:03 +0400

    made small files changes

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
-import { RestaurantsScreen } from "./src/screens/RestaurantsScreen";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/RestaurantsScreen";
 
 
 export default function App() {
