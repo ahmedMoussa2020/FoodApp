dd23d6b9d2724acfbf20abd56a7f197a1fa92270
Ahmed <>
2023-01-30 20:01:41 +0400

    view card almost done. needs to debug for the bed icon, because of not showing

--- src/features/restaurants/screens/RestaurantsScreen.js
+++ src/features/restaurants/screens/RestaurantsScreen.js
@@ -6,17 +6,18 @@ import styled from "styled-components";
 
 const SafeArea = styled(SafeAreaView)`
   flex: 1;
-  margin-top: ${StatusBar.currentHeight}px;
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
 `;
 
 const SearchContainer = styled.View`
-  padding: 16px;
+  padding: ${(props) => props.theme.space[3]};
 `;
 
 const RestaurantListContainer = styled.View`
   flex: 1;
-  padding: 16px;
-  background-color: blue;
+  padding: ${(props) => props.theme.space[3]}
+  background-color: pink;
+
 `;
 
 export const RestaurantsScreen = () => (
